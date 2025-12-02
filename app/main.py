ecb6d0e3cbd62991d918995e30ff6af2996b6f8d
georgekazz <sfinas1998@hotmail.com>
2025-12-02 12:38:42 +0200

    change middleware

--- app/main.py
+++ app/main.py
@@ -32,8 +32,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"], #prosorina
-    allow_origins=["*"],
+    allow_origins=["http://192.168.6.123:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
